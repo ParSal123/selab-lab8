977cc68f5517e93a0a08cb2c2ef7be410133d588
Parsa Salavati <parsa.salavati@gmail.com>
2022-06-03 22:19:20 +0430

    Add Sample Usecase for Clone Pattern

--- src/main/java/selab/App.java
+++ src/main/java/selab/App.java
@@ -11,5 +11,10 @@ public class App {
         Tree tree = factory.createTree();
         Flower flower = factory.createFlower();
         // Do whatever you want with the garden! :-)
+        // ...
+        Tree tree2 = (Tree) tree.clone();
+        Flower flower2 = (Flower) flower.clone();
+        // Do whatever you like with the clones! :-)
+        // ...
     }
 }
